4b21404270981a7618794c2a47feea14e95c05e9
Kleiton Marques <kleitonm.marques@gmail.com>
2020-04-30 19:34:16 -0300

    Retornando Repositorios do GIT

--- JsAssincrono/exercicios2.js
+++ JsAssincrono/exercicios2.js
@@ -2,7 +2,7 @@ var nome = document.querySelector('input.nome')
 var retorno = document.querySelector('ul.retorno')
 
 function retornaDados(json) {
-    console.log(json.length)
+    retorno.innerHTML = ""
     for(i=0; i < json.length; i++) {
         repositorios = document.createElement('li')
         repositorios.innerHTML = json[i].name
@@ -11,13 +11,11 @@ function retornaDados(json) {
 }
 
 function buscar() {
+    retorno.innerHTML = "Carregando..."
     var nomeBusca = nome.value
     fetch('https://api.github.com/users/' + nomeBusca + '/repos', {
         method: 'GET'
     })
     .then(response => response.json())
     .then(json => retornaDados(json))
-    .catch(function(error) {
-        console.warn(error)
-    })
 }
